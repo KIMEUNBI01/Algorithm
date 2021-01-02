2006c015793c02f190d2a6f39a21d90d64510bde
KIMEUNBI01 <72555010+KIMEUNBI01@users.noreply.github.com>
2021-01-03 01:35:58 +0900

    geometry

---
+++ 3009_FourthPoint.py
@@ -0,0 +1,16 @@
+X = [] #x축 좌표
+Y = [] #y축 좌표
+
+for i in range(3): #x,y 좌표쌍을 3개 입력받아 각각의 리스트에 append
+    _x, _y = map(int, input().split())
+    X.append(_x)
+    Y.append(_y)
+    
+for i in X: #x좌표 구하기
+    if X.count(i) == 1:  
+        x = i
+for i in Y: #y좌표 구하기
+    if Y.count(i) == 1:
+        y = i
+
+print(x,y)
