eda7a5d640f08db084d75e0bfc730fb5a3116ca9
KIMEUNBI01 <72555010+KIMEUNBI01@users.noreply.github.com>
2021-01-28 21:48:45 +0900

    Brute Force Algorithm

--- 7568_build.py
+++ 7568_build.py
@@ -6,7 +6,7 @@ People = [list(map(int, input().split())) for _ in range(N)]
 for i in range(N):
     rank = 1
     for j in range(N):
-        #키와 몸무게 두 값이 모두 큰 경우만 큰 덩치이다
+        #키와 몸무게 두 값이 모두 커야 큰 덩치이다
         if People[i][0] < People[j][0] and People[i][1] < People[j][1]:
                 rank += 1
     print(rank, end = " ")
