90a1436391dabc8a2b210be416414bac1fec1727
KIMEUNBI01 <72555010+KIMEUNBI01@users.noreply.github.com>
2021-02-25 22:25:53 +0900

    Backtracking

---
+++ 15652_NnM(4).py
@@ -0,0 +1,16 @@
+# 백준 15652 김은비
+
+N, M = map(int, input().split())
+Num = [] #수열을 담아둘 배열
+
+def DFS(x, y, N, M): 
+    if(x == M): #1부터 N까지 자연수 중에서 중복을 허용하여 M개를 고른 수열을 오름차순으로 출력한다
+        print(*Num)
+        return
+    
+    for i in range(y, N):
+        Num.append(i+1) #그리고 Num에 i+1을 append (인덱스는 0부터 시작하기 때문에 숫자를 구하기 위해선 1을 늘려준다)
+        DFS(x+1, i, N, M) #다음 깊이을 탐색한다 (고른 수열은 비내림차순이어야 하므로 i를 넘겨준다)
+        Num.pop() #사용한 배열은 지워준다
+            
+DFS(0, 0, N, M)
